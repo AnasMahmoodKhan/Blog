17d5446e14cdc2c234a078d42a58e9e6235f7007
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-03-05 17:31:10 +0530

    Basic Unit tests

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Navbar />
       <Homepage />
       {isSignedIn && <Blogs />}
+      
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { configure, shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { Provider } from "react-redux";
+import store from "./app/store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe("Testing Blog", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders Login With Google", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const linkElement = screen.getByText("Login With Google");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("matches the snapshot", () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
